3565ebbbb3a20f0478e2fb27a2983f6c249e33ca
luis-barrera <62255037+luis-barrera@users.noreply.github.com>
2020-09-25 08:44:37 -0500

    Add files via upload

--- qtile/config.py
+++ qtile/config.py
@@ -1,5 +1,5 @@
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#++++++++++++++++++++++++Imports y Variables++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++ Imports y Variables +++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #Imports
 from typing import List  # noqa: F401
@@ -10,29 +10,39 @@ from libqtile.utils import guess_terminal
 import os
 import subprocess
 from libqtile import hook
+
 #Variables globales
 mod = "mod4"
 alt = "mod1"
 terminal = "kitty"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#++++++++++++++++++++++++Keybindings++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++ Keybindings +++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 keys = [
     #--------------------------------------------------------
-    #--------------WM Control--------------------------------
+    #----------- Controles de Generales ---------------------
     #--------------------------------------------------------    
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), lazy.window.bring_to_front(),
+    # Cambiar entre ventanas
+    Key([mod], "h",
+        lazy.layout.left(),
+        #lazy.window.bring_to_front(),
         desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), lazy.window.bring_to_front(),
+    Key([mod], "l",
+        lazy.layout.right(),
+        #lazy.window.bring_to_front(),
         desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), lazy.group.prev_window(), lazy.window.bring_to_front(),
+    Key([mod], "j",
+        #lazy.layout.down(),
+        lazy.group.prev_window(),
+        #lazy.window.bring_to_front(),
         desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), lazy.group.next_window(), lazy.window.bring_to_front(),
+    Key([mod], "k",
+        #lazy.layout.up(),
+        lazy.group.next_window(),
+        #lazy.window.bring_to_front(),
         desc="Move focus up"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
+    # Mover ventanas a otros lugares del layout, funciona con solo algunos layouts
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
         desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
@@ -41,8 +51,27 @@ keys = [
         desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(),
         desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    # Otros keybindings, para otros tipos de layouts 
+    Key([mod, "shift"], "space",
+        lazy.layout.rotate(),
+        lazy.layout.flip()), # xmonad-tall
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()), # Stack, xmonad-tall
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()), # Stack, xmonad-tall
+    Key([mod, "control"], "l",
+        lazy.layout.add(),                # Stack
+        lazy.layout.increase_ratio(),     # Tile
+        lazy.layout.maximize()),           # xmonad-tall
+    Key([mod, "control"], "h",
+        lazy.layout.delete(),             # Stack
+        lazy.layout.decrease_ratio(),     # Tile
+        lazy.layout.normalize()),          # xmonad-tall
+    Key([mod, "control"], "k",
+        lazy.layout.shrink(),             # xmonad-tall
+        lazy.layout.decrease_nmaster()),   # Tile
+    Key([mod, "control"], "j",
+        lazy.layout.grow(),               # xmonad-tall
+        lazy.layout.increase_nmaster()),   # Tile
+    # Cambiar tamaño de las ventanas en alguno layouts que lo permiten 
     Key([mod, "control"], "h", lazy.layout.grow_left(),
         desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(),
@@ -53,9 +82,6 @@ keys = [
         desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(),
         desc="Reset all window sizes"),
-    # Switch window focus to other pane(s) of stack
-    Key([mod], "space", lazy.layout.next(),
-        desc="Switch window focus to other pane(s) of stack"),
     # Floating
     Key([mod], "a", lazy.window.toggle_floating(),
         desc = "Toggle Window's Floating Mode"),
@@ -88,12 +114,10 @@ keys = [
     Key([mod, alt], "9", lazy.to_screen(0), lazy.group.toscreen(0),
         desc="move to screen 1"),
     Key([mod, alt], "0", lazy.to_screen(1), lazy.group.toscreen(1),
-        desc="move to screen 2"),
-    #
-
+        desc="move to screen 2"), 
     
     #--------------------------------------------------------
-    #--------------Controles Multimedia----------------------
+    #------------- Controles Multimedia ---------------------
     #--------------------------------------------------------
     # Brightness
     # Necesario instalar xorg-xbacklight 
@@ -112,7 +136,7 @@ keys = [
         desc="Audio Control"),
 
     #--------------------------------------------------------
-    #--------------Launch Apps-------------------------------
+    #-------------- Launch Apps -----------------------------
     #--------------------------------------------------------
     # rofi menu
     Key([mod], "x", lazy.spawn("rofi -combi-modi window,drun,ssh -icons-theme 'Newaita' -show combi"),
@@ -122,119 +146,15 @@ keys = [
         desc="Launch dmenu"),
     # terminal
     Key([mod], "Return", lazy.spawn(terminal),
-        desc="Launch terminal")
+        desc="Launch terminal"),
+    # Firefox
+    Key([mod], "c", lazy.spawn("firefox"),
+        desc="Launch Firefox"),
 ]
 
-
-groups = [Group(i) for i in "1234567890"]
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-    ])
-
-layouts = [
-    # layout.Max(),
-    layout.Bsp(),
-    layout.Stack(num_stacks=2),
-    # Try more layouts by unleashing below layouts.
-    layout.Columns(),
-    layout.Matrix(),
-    layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    layout.Zoomy(),
-]
-
-widget_defaults = dict(
-    font='Source Code Pro',
-    fontsize=12,
-    padding=3,
-)
-
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.Image(
-                    filename = '~/Imágenes/icon.png'),
-                widget.GroupBox(),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                # widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={'launch': ("#ff0000", "#ffffff"),},
-                    name_transform=lambda name: name.upper(),),
-                # widget.TextBox("default config", name="default"),
-                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # widget.DF(),
-                #widget.Mpris2(
-                #    objname = 'org.mpris.MediaPlayer2.spotify',
-                #    name = "spotify"),
-                widget.Pomodoro(
-                    color_inactive = 'dc7e61'),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                widget.NetGraph(),
-                #widget.CapsNumLockIndicator(),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                # widget.CheckUpdates(),
-                widget.Systray(),
-                # Necesario instalar brightnessctl y conocer el dispositivo de backlight con
-                #   ls /sys/class/backlight/
-                #widget.Sep(
-                #    foreground = 'a4e282',
-                #    linewidth = 2,),
-                #widget.Backlight(
-                #    change_command = 'brightnessctl set 60%',
-                #    backlight_name = 'intel_backlight',
-                #    step = 5,
-                #    format = ' {percent:2.0%}',),
-                #widget.Sep(
-                #    foreground = 'a4e282',
-                #    linewidth = 2,),
-                #widget.Battery(
-                #    format = '{char}{percent:2.0%} {hour:d}:{min:02d}',
-                #    update_interval = 10,
-                #    low_foreground = 'FF0000',),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                widget.Clock(format='%d-%b %a %I:%M'),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                widget.CurrentLayout(),
-                widget.Sep(
-                    foreground = 'a4e282',
-                    linewidth = 2,),
-                widget.QuickExit(),
-            ],
-            28
-        ),
-    ),
-]
-
-# Drag floating layouts.
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++++ Mouse Bindings +++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
@@ -243,12 +163,113 @@ mouse = [
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++ Grupos ++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#groups = [Group(i) for i in ["1", "2", "3", "4", "5", "6", "7", "8"]]
+groups = [Group("1", spawn="", layout="bsp"),
+    Group("2", spawn=["typora", "pomotroid"], layout="floating"),
+    Group("3", spawn="", layout="bsp"),
+    Group("4", spawn="", layout="bsp"),
+    Group("5", spawn="", layout="bsp"),
+    Group("6", spawn="", layout="bsp"),
+    Group("7", spawn="", layout="bsp"),
+    Group("8", spawn="", layout="bsp"),
+#    Group("9", spawn="", layout="bsp")
+    ]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+        # switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++ Layouts +++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#Colores 
+border = dict(
+    border_focus='#f6832f',
+    border_normal='#b8b5db',
+    border_width=2,
+    margin = 5)
+
+layouts = [
+    layout.Bsp(**border),
+    layout.Stack(num_stacks=1, **border),
+    layout.Columns(**border),
+    layout.Matrix(**border),
+    layout.MonadTall(**border),
+    layout.Zoomy(**border),
+    layout.Tile(**border),
+    layout.Floating(**border),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Max(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+]
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++ Bar y Widgets ++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Widgets defaults
+widget_defaults = dict(
+    font='Source Code Pro',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+sep_defs = dict(foreground = 'bee12a', linewidth = 2)
+
+# Bar
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.Image(
+                    filename = '~/Imágenes/icon.png'),
+                widget.GroupBox(
+                    highlight_method='line',
+                    #highlight_color=['000000', 'dd8a6f'],
+                    this_current_screen_border='f64a32'),
+                widget.Sep(**sep_defs),
+                widget.TaskList(),
+                widget.Chord(
+                    chords_colors={'launch': ("#ff0000", "#ffffff"),},
+                    name_transform=lambda name: name.upper(),),
+                widget.Mpris2(name='spotify',
+                              stop_pause_text='',
+                              scroll_chars=None,
+                              display_metadata=['xesam:title', 'xesam:artist'],
+                              objname="org.mpris.MediaPlayer2.spotify"),
+                widget.Sep(**sep_defs),
+                widget.Systray(),
+                widget.Sep(**sep_defs),
+                widget.Clock(format='%d-%b %a %I:%M'),
+                widget.Sep(**sep_defs),
+                widget.CurrentLayout(), 
+            ],
+            28, opacity=0.8
+        ),
+    ),
+]
+
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 main = None  # WARNING: this is deprecated and will be removed soon
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = True
 cursor_warp = False
+
+# Floating Layout
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     {'wmclass': 'confirm'},
@@ -265,10 +286,11 @@ floating_layout = layout.Floating(float_rules=[
     {'wname': 'branchdialog'},  # gitk
     {'wname': 'pinentry'},  # GPG key password entry
     {'wmclass': 'ssh-askpass'},  # ssh-askpass
-    {'wmclass': 'kitty'},
+    # {'wmclass': 'kitty'},
     {'wmclass': 'zoom'},
+    {'wmclass': 'keepassxc'},
     {'wmclass': 'pavucontrol'},
-])
+], **border)
 auto_fullscreen = False
 focus_on_window_activation = "smart"
 
@@ -282,6 +304,11 @@ def autostart():
     subprocess.Popen(["dropbox"])
     subprocess.Popen(["pa-applet"])
     subprocess.Popen(["nm-applet"])
+    
+#@hook.subscribe.client_new
+#def modify_window(client):
+#    if (client.window.get_wm_transient_for() or client.window.get_wm_type() in floating_types):
+#        client.floating = True
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -292,4 +319,3 @@ def autostart():
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
-
