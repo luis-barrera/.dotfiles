673404e1c2742da50d33c7ddd826faeba1ec84dc
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2021-10-24 22:55:59 -0500

    Agrega config de emacs

---
+++ emacs/.emacs.d/elpa/undo-fu-20211007.306/undo-fu-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from undo-fu.el  -*- no-byte-compile: t -*-
+(define-package "undo-fu" "20211007.306" "Undo helper with redo" '((emacs "25.1")) :commit "71c474e29f6ad726386604a5058761892951782e" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://gitlab.com/ideasman42/emacs-undo-fu")
