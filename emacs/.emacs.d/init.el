4d827548dd44ce75e33c74778a85e1de0dbdc652
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2021-10-31 18:05:05 -0600

    Cambios en Emacs
    init.el:
    - Agrega y mejora comentarios
    - Arregla problemas con la columna de números
    - Elimina command-log-mode
    - Mejora el undo en evil-mode
    - Agrega templates a org-roam
    - Cambia los símbolos para los headings de org-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,11 +1,11 @@
-
+;; Quitar mensaje de startup
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1) ; Desactiva las scrollbars
-(tool-bar-mode -1) ; Desactiva la tool bar
-(tooltip-mode -1) ; Desactiva los tooltips
-(set-fringe-mode 10) ; TODO
-(menu-bar-mode -1) ; Desactiva el menubar
+(scroll-bar-mode -1) ;; Desactiva las scrollbars
+(tool-bar-mode -1) ;; Desactiva la tool bar
+(tooltip-mode -1) ;; Desactiva los tooltips
+;;(set-fringe-mode 10) ;; TODO
+(menu-bar-mode -1) ;; Desactiva el menubar
 
 ;; La pantalla parpadea cuando hay errores
 (setq visible-bell t)
@@ -14,17 +14,24 @@
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
 
 ;; Mostrar en la barra inferior la columna en la que está el cursor
-(column-number-mode)
+(column-number-mode t)
 ;; Columna de números
-(global-display-line-numbers-mode t)
+(global-display-line-numbers-mode 1)
 ;; Numeros de linea relativos
-; BUG: no está funcionando en buffers de org mode, a veces se desactiva en algunos otros
-;(setq display-line-numbers 'relative)
-
+;; BUG: no está funcionando en buffers de org mode, a veces se desactiva en algunos otros
+(setq display-line-numbers-type 'relative)
 ;; Deshabilitar la columna de números en algunos modos
-;(dolist (mode '(term-mode-hook
-;		shell-mode-hook))
-;  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(dolist (mode '(term-mode-hook
+ 		shell-mode-hook))
+   (add-hook mode (lambda () (display-line-numbers-mode 'nil))))
+;; Cortar lineas
+(global-visual-line-mode t)
+
+;; Abrir ventanas a la derecha y no debajo
+(setq split-height-threshold nil)
+(setq split-width-threshold 0)
+;(setq split-window-right)
+
 
 
 ;; Package administrator
@@ -35,8 +42,7 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-;; Mostrar los comandos que ejecutamos
-(use-package command-log-mode)
+
 
 ;; Auto-completado
 (use-package ivy
@@ -76,14 +82,14 @@
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
   ;; Temas favoritos
+  ;; Si queremos tener una lista completa de temas usar M-x counsel-load-theme RET
   ;(load-theme 'doom-one t)
-  (load-theme 'doom-acario-dark t)
-  ;(load-theme 'doom-gruvbox t)
+  ;(load-theme 'doom-acario-dark t)
+  (load-theme 'doom-gruvbox t)
   ;(load-theme 'doom-Iosvkem t)
   ;(load-theme 'doom-manegarm t)
   ;(load-theme 'doom-peacock t)
   ;(load-theme 'doom-tomorrow-night t)
-  ; Si queremos tener una lista completa de temas usar M-x counsel-load-theme RET
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -95,9 +101,6 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
-;; Cortar lineas
-(global-visual-line-mode t)
-
 ;; Paréntesis de colores
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -108,24 +111,6 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(undo-fu evil-numbers org-superstar visual-fill-column forge magit counsel-projectile projectile evil-collection general all-the-icons-completion treemacs-all-the-icons doom-themes helpful counsel ivy-rich which-key rainbow-delimiters org-evil command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
 
 ;; Muestra más info acerca de los comandos, así como su keybinding si está disponible
 (use-package ivy-rich
@@ -152,26 +137,6 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-
-; Keybindings
-;; Evil mode, capa de vim
-(setq evil-want-keybinding nil)
-(setq evil-want-integration t)
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
-(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
-(setq evil-ex-search-vim-style-regexp t)
-
-
-;; Configuración de keybindings de evil para varios modos
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
 ;; Leader key con <espacio>, como en vim
 (use-package general
   :config
@@ -180,19 +145,65 @@
 			  ;:prefix "SPC"
 			  :prefix "C-SPC")
 
-  ; Estos son ejemplos de cómo se definen los keybindings
+  ;; Estos son ejemplos de cómo se definen los keybindings
   (rune/leader-keys
    "t" '(:ignore t :which-key "toggles")
    "tt" '(counsel-load-theme :which-key "choose theme")))
 
 ;; Ayuda a repetir comandos
-;(use-package hydra)
+;; (use-package hydra)
+
+
+
+;; Keybindings
+;; Evil mode, capa de vim
+(setq evil-want-keybinding nil)
+(setq evil-want-integration t)
+(unless (package-installed-p 'evil)
+  (package-install evil))
+(require 'evil)
+(evil-mode 1)
+(define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+(define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+(setq evil-ex-search-vim-style-regexp t)
+(setq evil-in-single-undo t)
+
+;; Configuración de keybindings de evil para varios modos
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 ;; Cambiar entre buffers
 (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 
+;; Tener bloques de código sin perder el nivel de jerarquía
+; org-mode no deja que metas un subtítulo y luego regreses al título anterior
+; cosa que puedes hacer en LaTeX, pero aquí no. Para lograr algo similar está
+; este paquete que pertenece a org-mode pero está desactivado por defecto.
+; Para usarlo está el key =C-c C-x t=
+(require 'org-inlinetask)
 
-; Git y manejo de proyectos
+;; Incremento/Decremento como en vim
+(use-package evil-numbers
+  :after evil)
+(evil-define-key '(normal visual) 'global (kbd "+") 'evil-numbers/inc-at-pt)
+(evil-define-key '(normal visual) 'global (kbd "-") 'evil-numbers/dec-at-pt)
+
+;; Mejor soporte para undo y redo
+(use-package undo-fu
+  :after evil)
+(define-key evil-normal-state-map "u" 'undo-fu-only-undo)
+(define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo)
+
+;; Usa tecla ESC para cancelar comandos
+(define-key isearch-mode-map [escape] 'isearch-abort)
+(define-key isearch-mode-map "\e" 'isearch-abort)
+(global-set-key [escape] 'keyboard-escape-quit)
+
+
+
+;; Git y manejo de proyectos
 ;; Manejo de proyectos según le directorio en el que estamos
 (use-package projectile
   :diminish projectile-mode
@@ -202,7 +213,7 @@
   :init
   ; Si entramos a cualquier directorio dentro de ~/dev se carga una configuración global dentro de ese directorio
   (when (file-directory-p "~/dev")
-    (setq projectile-project-search-path '("~/dev")))
+  (setq projectile-project-search-path '("~/dev")))
   ; Cuando entramos a un directorio, dired también se mueva a ese directorio
   (setq projectile-switch-project-action #'projectile-dired))
 
@@ -212,60 +223,44 @@
 
 ;; Soporte para git y otros vcs
 ;; Debemos entrar a magit-status, con s podemos mover archivos al unstaged y con u los sacamos
-(use-package magit
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+;; (use-package magit
+;;   :custom
+;;   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package forge)
-
-;; Este paquete forma parte de evil-collection
-;(use-package evil-magit
-;  :after magit)
+;; Soporte de Magit para GitHub y GitLab
+;; (use-package forge)
 
 
-;; org mode
+
+;; Org mode
 (use-package org
   :hook (org-mode . org-indent-mode)
   :config
-  ; Guardar los buffers después de hacer un refile
+  ;; Guardar los buffers después de hacer un refile
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-  ; Seguir links al dar RET sobre ellos
+  ;; Seguir links al dar RET sobre ellos
   (setq org-return-follows-link t)
-
-  ; Abrir los links en el mismo frame
+  ;; Abrir los links en el mismo frame
   (setq org-link-frame-setup '((file . find-file)))
-
-  ; Símbolo a usar cuando cuando hay texto oculto por usar S-TAB
-  (setq org-ellipsis " ⥥")
-
-  ; Elimina los símbolos que se usan para modificar los caracteres, por ejemplo: los * que se usan para hacer negritas
-  ;org-hide-emphasis-markers t)
-
-  ; Org agenda
+  ;; Símbolo a usar cuando cuando hay texto oculto por usar S-TAB
+  (setq org-ellipsis "⥥")
+  ;; Elimina los símbolos que se usan para modificar los caracteres, por ejemplo: los * que se usan para hacer negritas
+  (setq org-hide-emphasis-markers t)
+  ;; Org agenda
   (setq org-agenda-files '("~/org-mode/tasks.org"))
-
-  ; Seguir links de org mode al dar RET sobre ellos
-  (setq org-return-follows-link t)
-
-  ; Keywords para tasks
+  ;; Keywords para tasks
   (setq org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE"))))
 
-;; Tener bloques de código sin perder el nivel de jerarquía
-; org-mode no deja que metas un subtítulo y luego regreses al título anterior
-; cosa que puedes hacer en LaTeX, pero aquí no. Para lograr algo similar está
-; este paquete que pertenece a org-mode pero está desactivado por defecto.
-; Para usarlo está el key =C-c C-x t=
-(require 'org-inlinetask)
+;; Seguir links de org mode al dar RET sobre ellos
+(setq org-return-follows-link t)
 
-
-; Cambiar los símbolos de los headings
+;; Cambiar los símbolos de los headings
 (use-package org-superstar
   :after org
   :hook (org-mode . org-superstar-mode)
   :config
   ; Caracteres a usar para cada nivel
-  (setq org-superstar-headline-bullets-list '("Ⅰ" "Ⅱ" "Ⅲ" "Ⅳ" "Ⅴ" "Ⅵ" "Ⅶ" "Ⅷ"))
+  (setq org-superstar-headline-bullets-list '("❉" "❋" "✺" "✹" "✸" "✶" "✵" "✲"))
   ; Al llegar más allá de 8 niveles, nil=usar siempre el último caracter, t=ciclar entre los elementos
   (setq org-superstar-cycle-headline-bullets 'nil)
   ; Ocultar un puntito que sale enfrente de los heading
@@ -273,31 +268,42 @@
   (setq org-superstar-leading-bullet ?\s)
   (setq org-indent-mode-turns-on-hiding-stars nil))
 
-;; Poner bordes a los lados del editor, solo en org-mode
-(use-package visual-fill-column
-  :defer t
-  :hook (org-mode . efs/org-mode-visual-fill))
-
 ;; Zettlekasten en org-mode
 (use-package org-roam
   :ensure t
   :init (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/org-roam")
+  (setq org-roam-directory "~/org-roam")
+  ;(setq org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
 	 ; Esta es la función con la que debemos empezar
 	 ("C-c n f" . org-roam-node-find)
 	 ("C-c n i" . org-roam-node-insert)
 	 ("C-M-i" . completion-at-point))
   :config
-  (org-roam-setup)
+  ;(org-roam-setup)
   (setq org-roam-completion-everywhere t))
 
-;; Incremento/Decremento como en vim
-(use-package evil-numbers
-  :after evil)
-(evil-define-key '(normal visual) 'global (kbd "+") 'evil-numbers/inc-at-pt)
-(evil-define-key '(normal visual) 'global (kbd "-") 'evil-numbers/dec-at-pt)
+;; Templates para org-roam
+(setq org-roam-capture-templates
+      '(("d" "default" plain "%?"
+	 :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+			    "#+title: ${title}\n")
+	 :unnarrowed t)
+	("c" "Nota completa" plain "%?"
+	 :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+			    ":PROPERTIES:
+:AUTHOR: %^{Author|No Author}
+:ROAM_REFS: %^{Link o Ref|No Ref}
+:ROAM_ALIAS: %^{Alias|No Alias}
+:TYPE: %^{Type|Articulo|Noticia|Libro|Video}
+:DATE: %T
+:CREATOR: luis-barrera
+:END:
+#+title: ${title}
+#+filetags:
+")
+	 :unnarrowed t)))
 
 ;; Evil para org, hay algunas teclas que no funcionan correctamente en org mode debido a evil
 (use-package evil-org
@@ -308,20 +314,10 @@
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
 
-;; Mejor soporte para undo y redo
-(use-package undo-fu)
-(define-key evil-normal-state-map "u" 'undo-fu-only-undo)
-(define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo)
-
-;; Usa tecla ESC para cancelar comandos
-(define-key isearch-mode-map [escape] 'isearch-abort)
-(define-key isearch-mode-map "\e" 'isearch-abort)
-(global-set-key [escape] 'keyboard-escape-quit)
-
-;; Abrir ventanas a la derecha y no debajo
-(setq split-height-threshold nil)
-(setq split-width-threshold 0)
-;(setq split-window-right)
+;; Poner bordes a los lados del editor, solo en org-mode
+(use-package visual-fill-column
+  :defer t
+  :hook (org-mode . efs/org-mode-visual-fill))
 
 ;; Poner bordes a los lados del editor, solo en org-mode
 (defun efs/org-mode-visual-fill ()
@@ -335,3 +331,31 @@
      (define-key keymap-from key nil))
 (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
 (my-move-key evil-motion-state-map evil-normal-state-map " ")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers 'relative)
+ '(package-selected-packages
+   '(undo-fu evil-numbers org-superstar visual-fill-column forge magit counsel-projectile projectile evil-collection general all-the-icons-completion treemacs-all-the-icons doom-themes helpful counsel ivy-rich which-key rainbow-delimiters org-evil command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.3))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.2))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1)))))
+
+;; Poner los archivos de Backup (los que terminan en ~) en otro lugar
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
