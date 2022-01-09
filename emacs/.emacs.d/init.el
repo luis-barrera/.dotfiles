fbd5e13283fd001ec3934d42db1dcbfc126d0619
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-01-09 09:50:02 -0600

    Cambios a Emacs
    - Agrega hooks para mostrar columna de números en LaTeX-mode
    - Quita el package-refresh-contents, usaba muchos recursos al iniciar
      Emacs.
    - Agrega plugins y configs necesarios para trabajar con LaTeX:
      - Hooks para outline-minor-mode, prettify-symbols, yas-minor, cdlatex.
      - Auctex, preview, cdlatex.
    - Plugin org-roam-ui.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -31,6 +31,8 @@
   (display-line-numbers-mode t)
   (setq display-line-numbers-type 'relative))
 (add-hook 'prog-mode-hook 'my-display-numbers-hook)
+(add-hook 'latex-mode-hook 'my-display-numbers-hook)
+(add-hook 'LaTeX-mode-hook 'my-display-numbers-hook)
 (add-hook 'text-mode-hook (display-line-numbers-mode 'nil))
 (add-hook 'org-mode-hook (display-line-numbers-mode 'nil))
 ;; Numeros de linea relativos
@@ -56,7 +58,7 @@
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(package-refresh-contents)
+;(package-refresh-contents)
 
 ; (unless (package-installed-p 'use-package)
 ;    (package-install 'use-package))
@@ -72,9 +74,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(company-posframe ((t (:family "FiraCode Nerd Font Mono" :height 0.8))))
- ;; '(mode-line ((t (:family "FiraCode Nerd Font Mono" :height 0.8))))
- ;; '(mode-line-inactive ((t (:family "FiraCode Nerd Font Mono" :height 0.8))))
  '(company-posframe ((t (:family "VictorMono Nerd Font Mono" :height 0.8))))
  '(mode-line ((t (:family "VictorMono Nerd Font Mono" :height 0.8))))
  '(mode-line-inactive ((t (:family "VictorMono Nerd Font Mono" :height 0.8))))
@@ -141,11 +140,11 @@
   ;; Temas favoritos
   ;; Si queremos tener una lista completa de temas usar M-x counsel-load-theme RET
   ;(load-theme 'doom-one t)
-  ;(load-theme 'doom-acario-dark t)
+  (load-theme 'doom-acario-dark t)
   ;(load-theme 'doom-gruvbox t)
   ;(load-theme 'doom-Iosvkem t)
   ;(load-theme 'doom-manegarm t)
-  (load-theme 'doom-peacock t)
+  ;(load-theme 'doom-peacock t)
   ;(load-theme 'doom-tomorrow-night t)
 
   ;; Enable flashing mode-line on errors
@@ -288,7 +287,8 @@
 ;; ##############################
 ;; Org mode
 (use-package org
-  :hook (org-mode . org-indent-mode)
+  :hook ((org-mode . org-indent-mode))
+         ;; (org-mode . org-toggle-pretty-entities)
   :config
   ;; Guardar los buffers después de hacer un refile
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
@@ -742,6 +742,208 @@
 (use-package company-box
   :hook (company-mode . company-box-mode))
 
+;; LaTeX en Emacs
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+(add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
+(add-hook 'LaTeX-mode-hook 'yas-minor-mode-on)
+(add-hook 'LaTeX-mode-hook 'cdlatex-mode)   ; with AUCTeX LaTeX mode
+(add-hook 'latex-mode-hook 'turn-on-cdlatex)   ; with Emacs latex mode
+
+(use-package latex
+  :ensure auctex
+  :hook ((LaTeX-mode . prettify-symbols-mode))
+  :bind (:map LaTeX-mode-map
+         ("C-S-e" . latex-math-from-calc))
+  :config
+  ;; Format math as a Latex string with Calc
+  (defun latex-math-from-calc ()
+    "Evaluate `calc' on the contents of line at point."
+    (interactive)
+    (cond ((region-active-p)
+           (let* ((beg (region-beginning))
+                  (end (region-end))
+                  (string (buffer-substring-no-properties beg end)))
+             (kill-region beg end)
+             (insert (calc-eval `(,string calc-language latex
+                                          calc-prefer-frac t
+                                          calc-angle-mode rad)))))
+          (t (let ((l (thing-at-point 'line)))
+               (end-of-line 1) (kill-line 0) 
+               (insert (calc-eval `(,l
+                                    calc-language latex
+                                    calc-prefer-frac t
+                                    calc-angle-mode rad))))))))
+
+(use-package preview
+  :after latex
+  :hook ((LaTeX-mode . preview-larger-previews))
+  :config
+  (defun preview-larger-previews ()
+    (setq preview-scale-function
+          (lambda () (* 1.25
+                   (funcall (preview-scale-from-face)))))))
+
+;; CDLatex settings
+(use-package cdlatex
+  :ensure t
+  :hook (LaTeX-mode . turn-on-cdlatex)
+  :bind (:map cdlatex-mode-map
+              ("<tab>" . cdlatex-tab)))
+
+;; Yasnippet settings
+(use-package yasnippet
+  :ensure t
+  :hook ((LaTeX-mode . yas-minor-mode)
+         (post-self-insert . my/yas-try-expanding-auto-snippets))
+  :config
+  (use-package warnings
+    :config
+    (cl-pushnew '(yasnippet backquote-change)
+                warning-suppress-types
+                :test 'equal))
+
+  (setq yas-triggers-in-field t)
+
+  ;; Function that tries to autoexpand YaSnippets
+  ;; The double quoting is NOT a typo!
+  (defun my/yas-try-expanding-auto-snippets ()
+    (when (and (boundp 'yas-minor-mode) yas-minor-mode)
+      (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
+        (yas-expand)))))
+
+;; CDLatex integration with YaSnippet: Allow cdlatex tab to work inside Yas
+;; fields
+(use-package cdlatex
+  :hook ((cdlatex-tab . yas-expand)
+         (cdlatex-tab . cdlatex-in-yas-field))
+  :config
+  (use-package yasnippet
+    :bind (:map yas-keymap
+           ("<tab>" . yas-next-field-or-cdlatex)
+           ("TAB" . yas-next-field-or-cdlatex))
+    :config
+    (defun cdlatex-in-yas-field ()
+      ;; Check if we're at the end of the Yas field
+      (when-let* ((_ (overlayp yas--active-field-overlay))
+                  (end (overlay-end yas--active-field-overlay)))
+        (if (>= (point) end)
+            ;; Call yas-next-field if cdlatex can't expand here
+            (let ((s (thing-at-point 'sexp)))
+              (unless (and s (assoc (substring-no-properties s)
+                                    cdlatex-command-alist-comb))
+                (yas-next-field-or-maybe-expand)
+                t))
+          ;; otherwise expand and jump to the correct location
+          (let (cdlatex-tab-hook minp)
+            (setq minp
+                  (min (save-excursion (cdlatex-tab)
+                                       (point))
+                       (overlay-end yas--active-field-overlay)))
+            (goto-char minp) t))))
+
+    (defun yas-next-field-or-cdlatex ()
+      (interactive)
+      "Jump to the next Yas field correctly with cdlatex active."
+      (if (bound-and-true-p cdlatex-mode)
+          (cdlatex-tab)
+        (yas-next-field-or-maybe-expand)))))
+
+;; Array/tabular input with org-tables and cdlatex 
+(use-package org-table
+  :after cdlatex
+  :bind (:map orgtbl-mode-map
+              ("<tab>" . lazytab-org-table-next-field-maybe)
+              ("TAB" . lazytab-org-table-next-field-maybe))
+  :init
+  (add-hook 'cdlatex-tab-hook 'lazytab-cdlatex-or-orgtbl-next-field 90)
+  ;; Tabular environments using cdlatex
+  (add-to-list 'cdlatex-command-alist '("smat" "Insert smallmatrix env"
+                                       "\\left( \\begin{smallmatrix} ? \\end{smallmatrix} \\right)"
+                                       lazytab-position-cursor-and-edit
+                                       nil nil t))
+  (add-to-list 'cdlatex-command-alist '("bmat" "Insert bmatrix env"
+                                       "\\begin{bmatrix} ? \\end{bmatrix}"
+                                       lazytab-position-cursor-and-edit
+                                       nil nil t))
+  (add-to-list 'cdlatex-command-alist '("pmat" "Insert pmatrix env"
+                                       "\\begin{pmatrix} ? \\end{pmatrix}"
+                                       lazytab-position-cursor-and-edit
+                                       nil nil t))
+  (add-to-list 'cdlatex-command-alist '("tbl" "Insert table"
+                                        "\\begin{table}\n\\centering ? \\caption{}\n\\end{table}\n"
+                                       lazytab-position-cursor-and-edit
+                                       nil t nil))
+  :config
+  ;; Tab handling in org tables
+  (defun lazytab-position-cursor-and-edit ()
+    ;; (if (search-backward "\?" (- (point) 100) t)
+    ;;     (delete-char 1))
+    (cdlatex-position-cursor)
+    (lazytab-orgtbl-edit))
+
+  (defun lazytab-orgtbl-edit ()
+    (advice-add 'orgtbl-ctrl-c-ctrl-c :after #'lazytab-orgtbl-replace)
+    (orgtbl-mode 1)
+    (open-line 1)
+    (insert "\n|"))
+
+  (defun lazytab-orgtbl-replace (_)
+    (interactive "P")
+    (unless (org-at-table-p) (user-error "Not at a table"))
+    (let* ((table (org-table-to-lisp))
+           params
+           (replacement-table
+            (if (texmathp)
+                (lazytab-orgtbl-to-amsmath table params)
+              (orgtbl-to-latex table params))))
+      (kill-region (org-table-begin) (org-table-end))
+      (open-line 1)
+      (push-mark)
+      (insert replacement-table)
+      (align-regexp (region-beginning) (region-end) "\\([:space:]*\\)& ")
+      (orgtbl-mode -1)
+      (advice-remove 'orgtbl-ctrl-c-ctrl-c #'lazytab-orgtbl-replace)))
+
+  (defun lazytab-orgtbl-to-amsmath (table params)
+    (orgtbl-to-generic
+     table
+     (org-combine-plists
+      '(:splice t
+                :lstart ""
+                :lend " \\\\"
+                :sep " & "
+                :hline nil
+                :llend "")
+      params)))
+
+  (defun lazytab-cdlatex-or-orgtbl-next-field ()
+    (when (and (bound-and-true-p orgtbl-mode)
+               (org-table-p)
+               (looking-at "[[:space:]]*\\(?:|\\|$\\)")
+               (let ((s (thing-at-point 'sexp)))
+                 (not (and s (assoc s cdlatex-command-alist-comb)))))
+      (call-interactively #'org-table-next-field)
+      t))
+
+  (defun lazytab-org-table-next-field-maybe ()
+    (interactive)
+    (if (bound-and-true-p cdlatex-mode)
+        (cdlatex-tab)
+      (org-table-next-field))))
+; Snippets
+;; Function that tries to autoexpand YaSnippets
+;; The double quoting is NOT a typo!
+;; (defun my/yas-try-expanding-auto-snippets ()
+;;   (when (bound-and-true-p 'yas-minor-mode)
+;;       (let ((yas-buffer-local-condition ''(require-snippet-condition . auto)))
+;;         (yas-expand))))
+
+;; Try after every insertion
+;; (add-hook 'post-self-insert-hook #'my/yas-try-expanding-auto-snippets)
+
+
 ;; Enable the www ligature in every possible major mode
 ;; Mover a otro path, causa problemas no abre emacs
 ; (use-package ligature
@@ -762,13 +964,16 @@
 ;                                      "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
 ;     (global-ligature-mode 't))
 
+(add-to-list 'load-path "~/.emacs.d/private/org-roam-ui")
+(load-library "org-roam-ui")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ui company-box parrot solaire-mode multiple-cursors visual-fill-column all-the-icons-completion treemacs-evil org-evil evil-org evil-numbers evil-smartparens treemacs-all-the-icons treemacs-magit treemacs-projectile smartparens comment-tags rainbow-delimiters yasnippet-snippets yasnippet emmet-mode php-mode web-mode lsp-java lsp-pyright lsp-treemacs lsp-mode company-php company-web alert pomm deft org-download org-superstar org-roam evil-collection doom-themes doom-modeline counsel-projectile projectile helpful which-key command-log-mode undo-fu company ivy-hydra ivy-rich forge magit general ivy counsel swiper use-package)))
+   '(org-roam-ui cdlatex company-auctex auctex lsp-ui company-box parrot solaire-mode multiple-cursors visual-fill-column all-the-icons-completion treemacs-evil org-evil evil-org evil-numbers evil-smartparens treemacs-all-the-icons treemacs-magit treemacs-projectile smartparens comment-tags rainbow-delimiters yasnippet-snippets yasnippet emmet-mode php-mode web-mode lsp-java lsp-pyright lsp-treemacs lsp-mode company-php company-web alert pomm deft org-download org-superstar org-roam evil-collection doom-themes doom-modeline counsel-projectile projectile helpful which-key command-log-mode undo-fu company ivy-hydra ivy-rich forge magit general ivy counsel swiper use-package)))
 
 ;; Indentación
 ;; Se recomienda usar también los comando tabify y untabify
