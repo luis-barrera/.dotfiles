d793dcdb58b1dc06a023fb6aed57c8260edf72a3
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-01-24 09:07:23 -0600

    Agrega config de fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+pfetch
+
+cat ~/.cache/wal/sequences
+wal -q --theme sexy-brewer
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
