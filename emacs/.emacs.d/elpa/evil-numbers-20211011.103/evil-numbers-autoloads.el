d32e0cfee6f15bfc0531b991c9c2e4690923444f
Luis Antonio Valdovinos Barrera <luisvaldovinosbarrera@gmail.com>
2022-01-27 17:35:31 -0600

    Cambios a Emacs
    - Elimina directorio elpa del remoto

--- emacs/.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; evil-numbers-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "evil-numbers" "../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers.el"
-;;;;;;  "020ab925073a25a3df84353d52425bac")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers.el
- (autoload 'evil-numbers/inc-at-pt "evil-numbers" nil t)
- (autoload 'evil-numbers/dec-at-pt "evil-numbers" nil t)
- (autoload 'evil-numbers/inc-at-pt-incremental "evil-numbers" nil t)
- (autoload 'evil-numbers/dec-at-pt-incremental "evil-numbers" nil t)
-
-;;;### (autoloads "actual autoloads are elsewhere" "evil-numbers"
-;;;;;;  "../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-numbers" '("evil-numbers-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/evil-numbers-20211011.103/evil-numbers.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-numbers-autoloads.el ends here
--- emacs/.emacs.d/elpa/shrink-path-20190208.1335/shrink-path-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; shrink-path-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads "actual autoloads are elsewhere" "shrink-path"
-;;;;;;  "../../../../../.emacs.d/elpa/shrink-path-20190208.1335/shrink-path.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/shrink-path-20190208.1335/shrink-path.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "shrink-path" '("shrink-path-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/shrink-path-20190208.1335/shrink-path-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/shrink-path-20190208.1335/shrink-path.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; shrink-path-autoloads.el ends here
