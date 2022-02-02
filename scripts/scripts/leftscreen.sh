47f9c97bbf550676e83114ebb3ae73dd99fe4e54
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-02-01 22:58:39 -0600

    Cambios a scripts
    - Modifica el script para dos pantallas.

--- scripts/scripts/leftscreen.sh
+++ scripts/scripts/leftscreen.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-declare awesomePID=$(pgrep awesome)
-
-xrandr --output eDP1 --primary --mode 1600x900 --pos 1024x0 --rotate normal --output DP1 --off --output DP2 --mode 1024x768 --pos 0x132 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off && \
-
-# Reinicia awesomeWM
-kill -HUP $awesomePID
+xrandr --output eDP1 --primary --mode 1600x900 --pos 1024x0 --rotate normal --output DP1 --off --output DP2 --mode 1024x768 --pos 0x0 --rotate normal --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
