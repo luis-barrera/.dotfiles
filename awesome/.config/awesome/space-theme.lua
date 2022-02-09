93fb9e25668c4fcd3f484ab128e5ea44e5357021
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-02-08 19:09:34 -0600

    Cambios wallpaper AwesomeWM

--- awesome/.config/awesome/space-theme.lua
+++ awesome/.config/awesome/space-theme.lua
@@ -31,9 +31,9 @@ local theme = {}
 
 -- Directorios por default
 theme.default_dir = require("awful.util").get_themes_dir() .. "default"
--- theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/mountain2.jpg"
+theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/mountain2.jpg"
 -- theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/f2.jpg"
-theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/faye.jpg"
+-- theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/faye.jpg"
 -- theme.wallpaper = os.getenv("HOME") .. "/.config/awesome/wallpapers/seeyouspacecowboy.png"
 -- theme.wallpaper = os.getenv("HOME") .. "/Pictures/suitsat1_nasa_2008.jpg"
 -- theme.wallpaper = os.getenv("HOME") .. "/Imágenes/wallpaper1.jpg"
