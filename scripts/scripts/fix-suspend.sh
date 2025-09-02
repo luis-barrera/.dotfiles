e9348d92120f5d5a541077cd1e360fd5194dd0d4
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2025-09-02 12:18:11 -0600

    scripts: new fix-suspend service using script

--- scripts/scripts/fix-suspend.sh
+++ scripts/scripts/fix-suspend.sh
@@ -1,8 +1,8 @@
-#! bin/bash
+#!/bin/bash
 
 echo EHC1 > /proc/acpi/wakeup
 echo XHCI > /proc/acpi/wakeup
 echo IGBE > /proc/acpi/wakeup
 echo EXP2 > /proc/acpi/wakeup
 
-cat /proc/acpi/wakeup
+#cat /proc/acpi/wakeup
